0dae959cb3dea1f34901c0f085ee509e40542402
Nikitha Gullapalli <Android2012>
2023-01-29 21:26:01 -0800

    viewBinding

--- app/src/main/java/com/example/customviewexample/MainActivity.kt
+++ app/src/main/java/com/example/customviewexample/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.customviewexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customviewexample.databinding.ActivityMainBinding
 import com.example.customviewexample.viewModel.MainViewModel
+
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         viewModel.getHtml()
     }
 }
--- app/src/main/java/com/example/customviewexample/bindingAdapters/htmlBindingAdapter.kt
+++ app/src/main/java/com/example/customviewexample/bindingAdapters/htmlBindingAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.customviewexample.bindingAdapters
 
+import android.os.Build
 import android.text.Html
 import android.text.SpannableString
+import androidx.annotation.RequiresApi
 import androidx.databinding.BindingAdapter
 import com.example.customviewexample.customViews.HtmlTextView
 
@@ -9,6 +11,7 @@ import com.example.customviewexample.customViews.HtmlTextView
  * This is how you deal with html in a TextView.  You have to render the html to a spannable string, which textview will handle.
  * https://stackoverflow.com/questions/37904739/html-fromhtml-deprecated-in-android-n
  * */
+@RequiresApi(Build.VERSION_CODES.N)
 @BindingAdapter("customHtmlTextview.htmlText")
 fun setHtmlText(htmlTextView: HtmlTextView, htmlText: String?) {
     htmlText?.let {
@@ -24,7 +27,7 @@ fun setHtmlText(htmlTextView: HtmlTextView, htmlText: String?) {
         htmlTextView.setHtmlString(
             SpannableString(
                 Html.fromHtml(
-                    "",
+                    "No html text received!",
                     Html.FROM_HTML_MODE_LEGACY
                 )
             )
